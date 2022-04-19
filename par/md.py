02d5088c527cd0cba7dd42439a0f49c258ef9377
smolthyme <matt@minnisites.com>
2022-04-19 23:26:49 +0200

    md - initial audiotag support for img type links

--- par/md.py
+++ par/md.py
@@ -540,6 +540,9 @@ class MarkdownHtmlVisitor(WikiHtmlVisitor):
             return self.tag('video', enclose=1, type="video/mp4", controls="yesplz", disablePictureInPicture=True,
                             playsinline="True", **kwargs)
 
+        elif src.endswith(".m4a") or src.endswith(".aac") or src.endswith(".ogg") or src.endswith(".oga") or src.endswith(".opus"):
+            return self.tag('audio', enclose=1, type="video/mp4", controls="yesplz", **kwargs)
+
         return self.tag('img', enclose=1, **kwargs)
 
     def visit_refer_link(self, node):
